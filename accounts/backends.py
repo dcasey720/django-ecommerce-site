6f8ee9b7ccf4926edcc2c73b10b9881e9cc39d3b
Daniel Casey <dcasey720@gmail.com>
2019-02-04 22:50:58 +0000

    Changed backends.py for usernae_or_email

--- accounts/backends.py
+++ accounts/backends.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 class EmailAuth:
     """Authenticate a user by an exact match on the email and password"""
 
-    def authenticate(self, username=None, password=None):
+    def authenticate(self, username_or_email=None, password=None):
         """
         Get an instance of `User` based off the email and verify the
         password
@@ -19,12 +19,12 @@ class EmailAuth:
             return None
         except User.DoesNotExist:
             return None
-    
+
     def get_user(self, user_id):
         """
         Used by the Django authentiation system to retrieve a user instance
         """
-        
+
         try:
             user = User.objects.get(pk=user_id)
 
