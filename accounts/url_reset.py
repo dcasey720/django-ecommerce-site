47b385df6300078333fdd0baaa4dbbfcb8f9c47f
Daniel Casey <dcasey720@gmail.com>
2019-02-05 11:38:37 +0000

    Changed post_reset_confirm to post_reset_redirect in url_reset.py.

--- accounts/url_reset.py
+++ accounts/url_reset.py
@@ -5,11 +5,7 @@ from django.contrib.auth.views import password_reset, password_reset_done, passw
 urlpatterns = [
     url('^$', password_reset, {'post_reset_redirect': reverse_lazy('password_reset_done')}, name='password_reset'),
     url(r'^done/$', password_reset_done, name='password_reset_done'),
-    url(r'^(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', password_reset_confirm, {'post_reset_confirm': reverse_lazy('password_reset_complete')},
+    url(r'^(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', password_reset_confirm, {'post_reset_redirect': reverse_lazy('password_reset_complete')},
         name='password_reset_confirm'),
     url('^complete/$', password_reset_complete, name='password_reset_complete')
 ]
-
-# this was in after 'password_rest_confirm, 'but was through error 'password_reset_confirm() got an unexpected keyword argument 'post_reset_confirm''
-# when the passwrod reset link was clicked
-#       {'post_reset_confirm': reverse_lazy('password_reset_complete')},
